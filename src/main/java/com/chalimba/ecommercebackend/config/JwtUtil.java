6754b530585075fa5bd59b53c47f98c0168792fc
Nico Feliciano Chalimba <chalimba@hm.edu>
2022-01-02 20:49:25 +0100

    refactor: refactored tokenprovider

--- src/main/java/com/chalimba/ecommercebackend/config/JwtUtil.java
+++ src/main/java/com/chalimba/ecommercebackend/config/JwtUtil.java
@@ -22,7 +22,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
-public class TokenProvider implements Serializable {
+public class JwtUtil implements Serializable {
 
     @Value("${jwt.token.validity}")
     public long TOKEN_VALIDITY;
@@ -58,7 +58,7 @@ public class TokenProvider implements Serializable {
         return expiration.before(new Date());
     }
 
-    public String generateToken(Authentication authentication) {
+    public String generateToken(Authentication authentication, long tokenValidity) {
         String authorities = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(","));
@@ -67,7 +67,7 @@ public class TokenProvider implements Serializable {
                 .setSubject(authentication.getName())
                 .claim(AUTHORITIES_KEY, authorities)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + TOKEN_VALIDITY * 1000))
+                .setExpiration(new Date(System.currentTimeMillis() + tokenValidity * 1000))
                 .signWith(SignatureAlgorithm.HS256, SIGNING_KEY)
                 .compact();
     }
