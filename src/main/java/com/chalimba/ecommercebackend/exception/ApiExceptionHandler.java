9f9fe0a843357c20b8607d0849981ac469642bc5
Nico Feliciano Chalimba <chalimba@hm.edu>
2022-01-04 00:22:02 +0100

    feat: added slf4j logging

--- src/main/java/com/chalimba/ecommercebackend/exception/ApiExceptionHandler.java
+++ src/main/java/com/chalimba/ecommercebackend/exception/ApiExceptionHandler.java
@@ -7,8 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import lombok.extern.slf4j.Slf4j;
 
 @ControllerAdvice
+@Slf4j
 public class ApiExceptionHandler {
 
     @ExceptionHandler(value = { BadRequestException.class })
@@ -23,14 +27,21 @@ public class ApiExceptionHandler {
 
     }
 
+    @ExceptionHandler(value = { NoHandlerFoundException.class })
+    public ResponseEntity<?> handleNoHandlerFoundException(NoHandlerFoundException e) {
+        return getExceptionResponse(HttpStatus.NOT_FOUND, e);
+
+    }
+
     @ExceptionHandler(value = { RuntimeException.class })
     public ResponseEntity<?> handleRuntimeException(RuntimeException e) {
         return getExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR, e);
     }
 
-    private ResponseEntity<?> getExceptionResponse(HttpStatus httpStatus, RuntimeException e) {
+    private ResponseEntity<?> getExceptionResponse(HttpStatus httpStatus, Exception e) {
         ApiExceptionDto apiExceptionDto = new ApiExceptionDto(e.getMessage(), httpStatus,
                 ZonedDateTime.now(ZoneId.of("Z")));
+        log.error(e.getMessage(), e);
         return ResponseEntity.status(httpStatus).body(apiExceptionDto);
     }
 
