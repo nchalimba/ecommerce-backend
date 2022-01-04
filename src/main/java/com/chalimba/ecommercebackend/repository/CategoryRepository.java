0184cc4c2dc372ec0d959c47b453c7b4ccc99017
Nico Feliciano Chalimba <chalimba@hm.edu>
2022-01-04 23:30:09 +0100

    bugfix: added repository annotations

--- src/main/java/com/chalimba/ecommercebackend/repository/CategoryRepository.java
+++ src/main/java/com/chalimba/ecommercebackend/repository/CategoryRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import com.chalimba.ecommercebackend.model.Category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByTitle(String title);
 }
--- src/main/java/com/chalimba/ecommercebackend/repository/CustomerRepository.java
+++ src/main/java/com/chalimba/ecommercebackend/repository/CustomerRepository.java
@@ -3,7 +3,9 @@ package com.chalimba.ecommercebackend.repository;
 import com.chalimba.ecommercebackend.model.Customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     long deleteByUserId(Long id);
 }
