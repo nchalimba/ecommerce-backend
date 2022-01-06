ae313e7f738e778dd9b3347668787dd0f78e2611
Nico Feliciano Chalimba <chalimba@hm.edu>
2022-01-07 00:29:56 +0100

    refactor: minor refactor

--- src/main/java/com/chalimba/ecommercebackend/config/JwtAuthenticationFilter.java
+++ src/main/java/com/chalimba/ecommercebackend/config/JwtAuthenticationFilter.java
@@ -9,11 +9,12 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.chalimba.ecommercebackend.service.UserService;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -34,7 +35,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Value("${jwt.token.validity.access-token}")
     private long ACCESS_TOKEN_VALIDITY;
 
-    private final UserDetailsService userDetailsService;
+    private final UserService userService;
     private final JwtUtil jwtUtil;
 
     @Override
@@ -75,7 +76,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
-            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            UserDetails userDetails = userService.loadUserByUsername(username);
 
             if (jwtUtil.validateToken(accessToken, userDetails)) {
                 UsernamePasswordAuthenticationToken authentication = jwtUtil.getAuthenticationToken(accessToken,
